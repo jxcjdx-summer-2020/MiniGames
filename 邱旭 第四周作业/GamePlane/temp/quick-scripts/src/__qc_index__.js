164b4814d7d7aeece62415dcaf08709b00eaa6e8
Eighroe <q978245488@163.com>
2020-08-17 00:06:09 +0800

    合并515

---
+++ 邱旭 第四周作业/GamePlane/temp/quick-scripts/src/__qc_index__.js
@@ -0,0 +1,11 @@
+
+require('./assets/migration/use_v2.0.x_cc.Toggle_event');
+require('./assets/script/app');
+require('./assets/script/bullet');
+require('./assets/script/end');
+require('./assets/script/enemy');
+require('./assets/script/hero');
+require('./assets/script/history');
+require('./assets/script/main');
+require('./assets/script/start');
+require('./assets/script/util');
