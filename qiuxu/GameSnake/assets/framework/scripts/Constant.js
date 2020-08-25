f2b16630ff26695cee5bc78434273276f32986ea
Eighroe <q978245488@163.com>
2020-08-25 14:00:40 +0800

    蛇2

--- qiuxu/GameSnake/assets/framework/scripts/Constant.js
+++ qiuxu/GameSnake/assets/framework/scripts/Constant.js
@@ -19,7 +19,6 @@ const Level_Speed = {
     ["Level_3"]: 0.16,
     ["Level_4"]: 0.14,
     ["Level_5"]: 0.12,
-    ["Level_6"]: 0.1,
 }
 
 // 关卡苹果基础生命值
@@ -31,16 +30,19 @@ const Level = cc.Enum({
     Level_3: 3,
     Level_4: 4,
     Level_5: 5,
-    Level_6: 6,
 });
 
 const Level_Score = cc.Enum({
-    Level_1: 30,
-    Level_2: 60,
-    Level_3: 90,
-    Level_4: 120,
-    Level_5: 200,
-    level_6: 9999,
+    // Level_1: 30,
+    // Level_2: 100,
+    // Level_3: 300,
+    // Level_4: 1000,
+    // Level_5: 9999,
+    Level_1: 10,
+    Level_2: 30,
+    Level_3: 60,
+    Level_4: 100,
+    Level_5: 9999,
 });
 
 // 最高记录密钥
@@ -55,5 +57,5 @@ module.exports = {
     Level,
     Level_Apple_Life_Time,
     Secret_Key,
-    Level_Score
+    Level_Score,
 }
