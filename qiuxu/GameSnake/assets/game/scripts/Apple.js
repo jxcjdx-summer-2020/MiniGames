1c2f109247210880d1f04c7e34a5868d973574d1
Eighroe <q978245488@163.com>
2020-08-25 13:56:07 +0800

    蛇2

---
+++ qiuxu/GameSnake/assets/game/scripts/Apple.js
@@ -0,0 +1,73 @@
+const BaseView = require("BaseView");
+const Enum = require("Constant");
+
+cc.Class({
+    extends: BaseView,
+
+    properties: {
+        
+    },
+
+    // LIFE-CYCLE CALLBACKS:
+
+    // onLoad () {},
+
+    start () {
+        
+    },
+
+    setGameLogic(logic) {
+        this._logic = logic;
+    },
+
+    init(posX, posY) {
+        this.reset();
+        this._super(posX, posY);
+        this.level = this._logic.level;
+        this.lifeTime = this.getCurrentLevelAppleLifeTime();
+        this.score = this.lifeTime * this.level;
+        this.schedule(this.appleTimer, 1);
+        return this;
+    },
+
+    getAppleScore() {
+        return this.score;
+    },
+
+    getAppleLifeTime() {
+        return this.lifeTime;
+    },
+
+    appleTimer() {
+        // 生命值判断
+        if (this.lifeTime <= 0) {
+            this.unschedule(this.appleTimer);
+            this._logic.initApple(this);
+            return;
+        }
+        // 3秒内闪烁
+        if (this.lifeTime == 3) {
+            this.addBlinkAnim();
+        }
+        if (this.score > this.level) {
+            this.score -= this.level;
+        }
+        this.lifeTime -= 1;
+    },
+
+    addBlinkAnim() {
+        this.node.runAction(cc.repeatForever(cc.blink(0.5, 1)));
+    },
+
+    reset() {
+        this.node.stopAllActions();
+        this.unscheduleAllCallbacks();
+        this.node.active = true;
+        this.node.opacity = 255;
+    },
+
+    getCurrentLevelAppleLifeTime() {
+        var origin_life_time = Enum.Level_Apple_Life_Time;
+        return Math.floor(Math.random() * origin_life_time / 2) + Math.floor(origin_life_time / 2 + 1);
+    },
+});
